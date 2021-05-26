9f69f468d63830dde40f7dc53d3fe72b38078c2e
Abdelrahman <said.abdelrahman89@gmail.com>
2021-05-26 02:52:33 +0100

    Swapped the order in the schema

--- api/schemas.js
+++ api/schemas.js
@@ -2,13 +2,13 @@ const mongoose = require("mongoose");
 
 const issueSchema = new mongoose.Schema(
     {
+        assigned_to: { type: String },
+        status_text: { type: String },
+        open: { type: Boolean, default: true },
         issue_title: { type: String, required: true },
         issue_text: { type: String, required: true },
         created_by: { type: String, required: true },
         project: { type: String, required: true },
-        assigned_to: { type: String },
-        open: { type: Boolean, default: true },
-        status_text: { type: String },
     },
     { timestamps: { createdAt: "created_on", updatedAt: "updated_on" } }
 );
